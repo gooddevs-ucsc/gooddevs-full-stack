bffc5c3f9974ec8dc9d49862de8391ae4e50ab40
chathurinadeesha <chathurinadeesha148@gmail.com>
2025-07-23 18:52:51 +0530

    set sponsor-root

--- frontend/src/app/routes/sponsor/root.tsx
+++ frontend/src/app/routes/sponsor/root.tsx
@@ -1,4 +1,4 @@
-import { Home, DollarSign } from 'lucide-react';
+import { Home, DollarSign, Settings } from 'lucide-react';
 import { Outlet } from 'react-router';
 
 import { DashboardLayout, type SideNavigationItem } from '@/components/layouts';
@@ -18,6 +18,11 @@ const SponsorRoot = () => {
       name: 'My Donations & Sponsorships',
       to: paths.sponsor.donationsSponshorships.getHref(),
       icon: DollarSign,
+    },
+    {
+      name: 'Settings',
+      to: paths.sponsor.settings.getHref(),
+      icon: Settings,
     },
   ] as SideNavigationItem[];
 
