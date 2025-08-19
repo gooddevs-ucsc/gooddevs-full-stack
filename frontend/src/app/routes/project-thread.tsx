32fa3f5127e93046272446db113de6bf9c86f245
Peshani-Rasangika <prranaweeraos2001@gmail.com>
2025-08-19 10:49:55 +0530

    Remove unused imports from ProjectThreadRoute for cleaner code

--- frontend/src/app/routes/project-thread.tsx
+++ frontend/src/app/routes/project-thread.tsx
@@ -1,8 +1,5 @@
 import { useParams } from 'react-router';
 
-import { Head } from '@/components/seo';
-import { Footer } from '@/components/ui/footer/footer';
-import { Navbar } from '@/components/ui/navbar/navbar';
 import { ProjectThread } from '@/features/projects/components/project-thread';
 
 const ProjectThreadRoute = () => {
