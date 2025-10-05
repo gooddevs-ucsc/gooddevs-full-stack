dc81129c967789c26366fe5bf81e74bc3fed3693
yomal7 <yomaloshada45@gmail.com>
2025-10-06 03:02:52 +0530

    Update notification exports to use specific API functions

--- frontend/src/features/notifications/index.ts
+++ frontend/src/features/notifications/index.ts
@@ -1,4 +1,6 @@
-export * from '@/features/notifications/api/notifications-api';
+export * from '@/features/notifications/api/get-notifications-list';
+export * from '@/features/notifications/api/get-unread-notifications';
+export * from '@/features/notifications/api/mark-as-read';
 export * from '@/features/notifications/components/notification-bell';
 export * from '@/features/notifications/components/notification-item';
 export * from '@/features/notifications/components/notifications-list';
